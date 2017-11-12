1d123a3013a65a16cb9688167456fa1b05d3d19b
Jacek <jace3k@gmail.com>
2017-11-12 16:21:15 +0100

    v1

--- src/jack/CryptMessageBehavior.java
+++ src/jack/CryptMessageBehavior.java
@@ -1,18 +1,21 @@
 package jack;
 
 public class CryptMessageBehavior implements CryptingBehaviors {
+    char xorKey;
+
     @Override
     public String crypt(String msg, String key) {
         StringBuilder cryptedMsg = new StringBuilder();
         int keyPos = 0;
+        xorKey = key.charAt(key.length()-1);
 
         for(int i = 0; i < msg.length(); i++) {
             char cryptedChar = (char) (msg.charAt(i) + key.charAt(keyPos));
-            cryptedMsg.append(cryptedChar);
+            char xorChar = (char) (cryptedChar ^ xorKey);
+
+            cryptedMsg.append(xorChar);
             keyPos++;
-            if(keyPos==key.length()) {
-                keyPos = 0;
-            }
+            if(keyPos == key.length()) keyPos = 0;
         }
         return cryptedMsg.toString();
     }
--- src/jack/DecryptMessageBehavior.java
+++ src/jack/DecryptMessageBehavior.java
@@ -1,16 +1,21 @@
 package jack;
 
 public class DecryptMessageBehavior implements CryptingBehaviors {
+    char xorKey;
+
     @Override
     public String crypt(String msg, String key) {
         StringBuilder decryptedMsg = new StringBuilder();
         int keyPos = 0;
+        xorKey = key.charAt(key.length()-1);
 
         for(int i = 0; i < msg.length(); i++) {
-            char decryptedChar = (char) (msg.charAt(i) - key.charAt(keyPos));
+            char xorChar = (char) (msg.charAt(i) ^ xorKey);
+            char decryptedChar = (char) (xorChar - key.charAt(keyPos));
+
             decryptedMsg.append(decryptedChar);
             keyPos++;
-            if(keyPos==key.length()) keyPos = 0;
+            if(keyPos == key.length()) keyPos = 0;
         }
         return decryptedMsg.toString();
     }
